745b79f0bcee8afb02b51db50ce820f6b0df2bf2
grin network <grinnetwork@protonmail.com>
2021-03-09 20:38:53 +0300

    added favicon and head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,40 @@
 import React from "react";
-
+import Head from "next/head";
 import { Container, Header, Main, Footer } from "@components";
 
 const Home: React.FC = () => {
     return (
-        <Container>
-            <Header />
-            <Main />
-            <Footer />
-        </Container>
+        <div>
+            <Head>
+                <title>Grin Network | Let's raise Grin</title>
+                <link
+                    rel="shortcut icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/icons/favicon-16x16.png"
+                />
+                <link
+                    rel="shortcut icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/icons/favicon-32x32.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/icons/apple-touch-icon.png"
+                />
+                <meta
+                    name="viewport"
+                    content="initial-scale=1.0, width=device-width"
+                />
+            </Head>
+            <Container>
+                <Header />
+                <Main />
+                <Footer />
+            </Container>
+        </div>
     );
 };
 
